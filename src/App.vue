41f561645a3157dfa1cf0388576d975064e1fcd6
Faiz Ainur Rofiq <cavadevop@gmail.com>
2021-06-07 21:48:17 +0700

    add .env file

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <v-app>
     <v-main>
-      <router-view>
-      </router-view>
+      <router-view> </router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
   }),
+  mounted() {
+    // console.log("env" + process.env.VUE_APP_FIREBASE_API_KEY);
+  },
 };
-
 </script>
--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import firebase from 'firebase';
-import 'firebase/firestore';
-import store from './store';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
+import "firebase/firestore";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 var firebaseConfig = {
-  apiKey: "AIzaSyC_evFjyIO9gCs1KmOb74mkCdC8YvcRJQY",
-  authDomain: "imblock-alpha.firebaseapp.com",
-  projectId: "imblock-alpha",
-  storageBucket: "imblock-alpha.appspot.com",
-  messagingSenderId: "953475870744",
-  appId: "1:953475870744:web:d784a4a7eee53e3b15136d",
-  measurementId: "G-7G66FMFB34"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -25,12 +25,12 @@ export default db;
 firebase.analytics();
 
 firebase.auth().onAuthStateChanged((user) => {
-  store.commit("updateUser", {user});
+  store.commit("updateUser", { user });
 });
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
